997b7fc09bc6b5442440fb6065cdc69eff16442d
Thomas English <thomas.english@joinzoe.com>
2023-12-04 13:12:55 +0000

    Day 02 Solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,6 @@ fun main(args: Array<String>) {
 
     println("Day 01 - Part 01: ${Day01().solvePartOne()}")
     println("Day 01 - Part 02: ${Day01().solvePartTwo()}")
+    println("Day 02 - Part 01: ${Day02().solvePartOne()}")
+    println("Day 02 - Part 02: ${Day02().solvePartTwo()}")
 }
